373a842b5b45108cc9586862e2b765d74628ee1e
green224 <green224@users.noreply.github.com>
2022-08-26 19:23:39 +0900

    ゲームページを更新：過去作品をもろもろ追加

--- games/main.js
+++ games/main.js
@@ -35,20 +35,38 @@ console.log(lang);
 			lang = languageStr == "ja" ? "jp" : "en";
 		}
 
+		// 言語バーを生成
+		let langBarHtml = "";
+		if (lang == "jp") {
+			langBarHtml =
+				'Japanese / <a href="' +
+				location.href.replace(/\?.*$/,"") + '?lang=en' +
+				'">English</a>';
+		} else {
+			langBarHtml =
+				'<a href="' +
+				location.href.replace(/\?.*$/,"") + '?lang=jp' +
+				'">日本語</a> / English';
+		}
+		$("#langSwitch")[0].innerHTML = langBarHtml;
+
+
 		// rootDataを読み込み
-		const rootData = JSON.parse( await this.loadFileText("root.json") );
+		const rootData = await this.loadFile("root.json");
 		this._rootData = rootData;
 
 		// ページを生成
-		let elemsHtml = "";
+		let elemsHtml = '<div class="divLine"></div>';
 		for (let page of rootData.pages) {
-			let locDataName = "loc_" + lang + "_" + page.id + ".json";
-			const locData = JSON.parse( await this.loadFileText(locDataName) );
+
+			const locDataName = "loc_" + lang + "_" + page.id + ".json";
+			const locData = await this.loadFile(locDataName);
+			if (locData == null) continue;
 
 			elemsHtml +=
 				'<div class="elemBox">' +
 					'<div class="detailTextArea">' +
-						'<div style="margin:4px; font-weight: bold; font-size: large;">' +
+						'<div style="margin:14px; font-weight: bold; font-size: large;">' +
 							'☆　' + locData.name + '　☆' +
 						'</div>' +
 						locData.desc +
@@ -56,28 +74,55 @@ console.log(lang);
 					'<div style="width: 100%; display: flex; align-items: stretch;">' +
 						'<div style="width: 50%; text-align: left; display: flex; align-items: flex-end; font-size: smaller;">' +
 							(lang=="jp" ? "プラットフォーム" : "Platform") + '：' + page.platform + '　' +
-							(lang=="jp" ? "プレイ時間" : "Game Time") + '：' + page.gameTime +
+							(lang=="jp" ? "プレイ時間" : "Game Time") + '：' + page.game_time +
 						'</div>' +
 						'<div style="width: 50%; text-align: right;">' +
-							'<a class="downloadLink" href="'+locData.dl_url+'">' +
-								(lang=="jp" ? "ダウンロード" : "DOWNLOAD") +
-							'</a>' +
+							(locData.dl_url=="" ? (
+								'<span class="downloadLinkClosed">' +
+									(lang=="jp" ? "公開終了" : "closed") +
+								'</span>'
+							) : (
+								'<a class="downloadLink" href="'+locData.dl_url+'">' +
+									(locData.dl_type=="download"
+										? (lang=="jp" ? "ダウンロード" : "DOWNLOAD")
+										: (lang=="jp" ? "ブラウザでプレイ" : "PLAY in Browser")
+									) +
+								'</a>'
+							) ) +
 							'<div style="font-size: smaller;">' +
 								(lang=="jp" ? "更新日" : "Update Date") + '：' + page.date +
 							'</div>' +
 						'</div>' +
 					'</div>' +
-					'<div class="mainSSCtnr"><img class="mainSS" src="'+page.ss+'"></div>' +
-				'</div>';
+					'<div class="mainSSCtnr">' +
+						(page.ss_type=="image"
+							? '<img class="mainSS" src="'+page.ss+'">'
+							: '<video class="mainSS" width="650" width="500" controls loop preload="metadata" playsinline src="'+page.ss+'">'
+						) +
+					'</div>' +
+				'</div>' +
+				'<div class="divLine"></div>';
 		};
 		$("#elemsRoot")[0].innerHTML = elemsHtml;
 	},
 
-	// 何らからのテキストファイルを読み込む処理
-	async loadFileText(filename) {
-		const res = await fetch(filename);
-		const text = await res.text();
-		return text;
-	},
+	// ファイルを読み込む
+	async loadFile(filename) {
+		return await new Promise( (resolve, reject) => {
+			$.ajax({
+				url: filename,
+				type: "GET",
+				async: true,
+			}).done(	// 正常終了
+				(result) => {
+					resolve(result);
+				}
+			).fail(		// エラー
+				() => {
+					resolve(null);
+				}
+			)
+		} );
+	}	
 
 };
